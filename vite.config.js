61d515a58c66d110f0205750a58208f810250438
Ryu Tae Hyun <ryu200112@gmail.com>
2025-03-07 19:36:31 +0900

    제발 돼라

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/r3f-vite-starter/", // GitHub Pages 경로 설정 (필수)
+  base: "/r3f-study5/", // GitHub Pages 경로 설정 (필수)
 });
